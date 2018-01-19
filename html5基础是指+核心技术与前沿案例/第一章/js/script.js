ce28d93204d5ad15ea2fae87f5b8e13fba13f947
xiaotian <952822399@qq.com>
2018-01-19 20:32:52 +0800

    第一章

---
+++ html5基础是指+核心技术与前沿案例/第一章/js/script.js
@@ -0,0 +1,7 @@
+var enroll= document.getElementById("enroll")
+enroll.onclick = function(e){
+    e.preventDefault();
+    enroll.innerHTML = '报名成功'
+    enroll.style.background = '#27cbl8b'
+    enroll.style.backgroundColor = '#27cb8b'
+}
