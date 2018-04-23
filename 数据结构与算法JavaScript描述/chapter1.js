72fab60229fb07f9e0c356c9c6f98a053e31c7f0
xiaotiandada <952822399@qq.com>
2018-04-23 17:38:59 +0800

    chapter1

---
+++ 数据结构与算法JavaScript描述/chapter1.js
@@ -0,0 +1,54 @@
+// 1.2.2
+
+// var x = 3
+// var y = 1.1
+// console.log(x+y)
+// console.log(x*y)
+// console.log((x+y)*(x-y))
+// var z = 9
+// console.log(Math.sqrt(z))
+// console.log(Math.abs(y/x))
+
+// var x = 3
+// var y = 1.1
+// var z = x * y
+// console.log(z.toFixed(2))
+
+
+// 1.2.3
+
+// var mid = 25
+// var high = 50
+// var low = 1
+// var current = 123
+// var found = -1
+// if(current < mid){
+//     mid = (current-low)/2
+// }
+
+// var mid = 25
+// var high = 50
+// var low = 1
+// var current = 123
+// var found = -1
+// if(current < mid){
+//     mid = (current-low)/2
+// } else {
+//     mid = (current+high) /2
+// }
+
+// var mid = 25
+// var high = 50
+// var low = 1
+// var current = 123
+// var found = -1
+// if(current < mid){
+//     mid = (current-low)/2
+//     console.log(1)
+// } else if(current > mid){
+//     mid = (current + high)/2
+//     console.log(2)    
+// } else {
+//     found = current
+//     console.log(3)    
+// }
